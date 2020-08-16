f27f64f227931996a61bbb38bcd8dd418b95ebee
Tej Charfi <charfi@usc.edu>
2020-08-16 20:32:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,8 +12,12 @@ def monopoly_with_second_tier
   # complexity.
   
   h={
-    :railroads=> {
-      :pieces=>4}  
+      :railroads=> {
+        :pieces=>4,
+        :rent_in_dollars=> {},
+        :names=> {}
+      
+      }  
     }
         
        
