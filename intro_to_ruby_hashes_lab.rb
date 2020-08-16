80411a420f6816bb9181caa56d8b741246b95369
Tej Charfi <charfi@usc.edu>
2020-08-16 20:10:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,7 +11,7 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   
-   h={:railroads=> :pieces=> 4, :rent_in_dollars=>{}, :names=> {}}
+   h={:railroads=> {:pieces=> 4, :rent_in_dollars=>{}, :names=> {}}
 
 end
 
@@ -20,6 +20,8 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  
+  
 
 end
 
